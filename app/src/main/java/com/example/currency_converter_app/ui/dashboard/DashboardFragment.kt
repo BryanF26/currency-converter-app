f1a25518d689162bf7fb99268d46ca65f051bef9
tasyaraishaniaa <tasyaraishania@gmail.com>
2024-10-01 17:14:18 +0700

    currency-converter-ui

--- app/src/main/java/com/example/currency_converter_app/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/currency_converter_app/ui/dashboard/DashboardFragment.kt
@@ -1,42 +1,44 @@
 package com.example.currency_converter_app.ui.dashboard
 
+import DashboardViewModel
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.currency_converter_app.databinding.FragmentDashboardBinding
+import com.example.currency_converter_app.R
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var spinner1: Spinner
+    private lateinit var spinner2: Spinner
+    private lateinit var viewModel: DashboardViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        spinner1 = view.findViewById(R.id.spinner1)
+        spinner2 = view.findViewById(R.id.spinner2)
+
+        val currencyList = listOf(
+            Currency("USD", R.drawable.flag_us),
+            Currency("EUR", R.drawable.flag_eur),
+            Currency("IDR", R.drawable.flag_ind)
+        )
+
+        val adapter = CurrencyAdapter(requireContext(), currencyList)
+        spinner1.adapter = adapter
+        spinner2.adapter = adapter
     }
 }
